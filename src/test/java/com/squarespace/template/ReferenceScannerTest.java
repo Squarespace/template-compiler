d6aea16a5c609c69ef0b850c9314bf975b1f6f79
Patrick Hensley <spaceboy@indirect.com>
2014-07-22 13:25:47 -0400

    [template] More reference scanner unit tests.

--- src/test/java/com/squarespace/template/ReferenceScannerTest.java
+++ src/test/java/com/squarespace/template/ReferenceScannerTest.java
@@ -34,19 +34,50 @@ public class ReferenceScannerTest extends UnitTestBase {
   }
 
   @Test
+  public void testFormatters() throws CodeException {
+    ObjectNode result = scan("{a|json|html|json}{b|json}{c|html}");
+    render(result);
+    assertEquals(result.get("formatters").get("json").asInt(), 3);
+    assertEquals(result.get("formatters").get("html").asInt(), 2);
+  }
+
+  @Test
+  public void testInstructions() throws CodeException {
+    ObjectNode result = scan("{.if a || b}{.newline}{.meta-left}{.space}{.or}{.section a}{.end}{.end}");
+    render(result);
+  }
+
+  @Test
   public void testSectionNesting() throws CodeException {
     ObjectNode result = scan("{.section a}{.section b}{.section c}{d}{.end}{.end}{.end}");
     render(result);
 
     ObjectNode vars = (ObjectNode)result.get("variables");
     assertTrue(vars.get("a").get("b").get("c").get("d").isNull());
+
+    result = scan("{.section a}{.end}{.section a}{.end}");
+    render(result);
+    assertTrue(vars.get("a").isObject());
+  }
+
+  @Test
+  public void testTextBytes() throws CodeException {
+    ObjectNode result = scan("{.section a}abcde{.or}fghij{.end}");
+    render(result);
+    assertEquals(result.get("textBytes").asInt(), 10);
   }
 
   @Test
   public void testAlternates() throws CodeException {
-    ObjectNode result = scan("{.odd? a}{a}{.or}{b}{.end}{.repeated section c}{.alternates with}d{.end}");
+    ObjectNode result = scan("{.odd? a}{a}{.or}{b}{.end}{.repeated section c}{.alternates with}{d}{.end}");
     render(result);
 
+    ObjectNode vars = (ObjectNode)result.get("variables");
+    assertTrue(vars.get("a").isNull());
+    assertTrue(vars.get("b").isNull());
+    assertTrue(vars.get("c").isObject());
+    assertEquals(((ObjectNode)vars.get("c")).size(), 1);
+    assertTrue(vars.get("c").get("d").isNull());
   }
 
   private ObjectNode scan(String source) throws CodeException {
