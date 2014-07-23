1b3f419944908caa805703c4cb6bba0c85a95545
Patrick Hensley <spaceboy@indirect.com>
2014-07-23 08:16:49 -0400

    [opensource] Updated license script, generated headers.

--- conf/license.sh
+++ conf/license.sh
@@ -4,7 +4,7 @@ CWD=$(cd `dirname $0`; pwd)
 GIT=$CWD/..
 
 for f in `find $GIT/src -name '*.java'` ; do
-    if ! grep -q 'Licensed to the Apache' $f ; then
+    if ! grep -q '* Licensed under the Apache License' $f ; then
         cat $CWD/apache-2.0-header.txt $f >$f.new && mv $f.new $f
     fi
 done
