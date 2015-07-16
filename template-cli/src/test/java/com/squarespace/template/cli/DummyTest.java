2e8a69e1bc0f269cf5b1a87cb8846932f7c35530
Patrick Hensley <pathensley@gmail.com>
2015-07-16 14:28:35 -0400

    Now travis-ci pushes to coveralls.
    Added a dummy test case to template-cli project, otherwise no coverage
    information will be produced.

---
+++ template-cli/src/test/java/com/squarespace/template/cli/DummyTest.java
@@ -0,0 +1,33 @@
+/**
+ *  Copyright, 2015, Squarespace, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.squarespace.template.cli;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ * Placeholder to ensure that Jacoco test report runs. It will fail to run if
+ * any subproject has zero tests.
+ */
+public class DummyTest {
+
+  @Test
+  public void testDummy() {
+    Assert.assertTrue(true);
+  }
+
+}
